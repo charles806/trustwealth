48bddfd99a38f52fec01817ad927a4b3549cd882
charles806 <c08445333@gmail.com>
2026-09-17 01:15:07 +0100

    KDKD

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
+declare(strict_types=1);
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
-declare(strict_types=1);
+
 
 require __DIR__ . '/app/config.php';
 require __DIR__ . '/app/auth.php';
